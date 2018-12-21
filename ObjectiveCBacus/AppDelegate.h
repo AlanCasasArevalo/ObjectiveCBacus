16f9459020f6fb5acc39a689294b3e04965b099b
Alan Casas <alancasasarevalo@gmail.com>
2018-12-21 19:07:32 +0100

    first commit

---
+++ ObjectiveCBacus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveCBacus
+//
+//  Created by Alan Casas on 21/12/18.
+//  Copyright © 2018 Alan Casas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
