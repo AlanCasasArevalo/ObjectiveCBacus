7141417e8658a5069df9e95a0eb257bb0f56eaad
Alan Casas <alancasasarevalo@gmail.com>
2018-12-24 09:52:07 +0100

    [Evolution]
    We created a model to view something in our view.

--- ObjectiveCBacus/AppDelegate.m
+++ ObjectiveCBacus/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "WineModel.h"
+#import "WineViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,9 +20,25 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor orangeColor];
     [self.window makeKeyAndVisible];
-    UIViewController* rootViewController = [[UIViewController alloc]init];
     
-    self.window.rootViewController = rootViewController;
+    // Creamos un modelo
+    UIImage* winePhoto = [UIImage imageNamed:@"bembibre.jpg"];
+    
+    NSURL* wineCompanyWeb = [NSURL URLWithString:@"http://www.dominiodetares.com/index.php/es/vinos/baltos/74-bembibrevinos"];
+    
+    WineModel* bembibre = [WineModel wineWithName:@"Bembibre"
+                                  wineCompanyName:@"Dominio de Tares"
+                                             type:@"tinto"
+                                            notes:@"Este vino muestra toda la complejidad y la elegancia de la variedad Mencía. En fase visual luce un color rojo picota muy cubierto con tonalidades violáceas en el menisco. En nariz aparecen recuerdos frutales muy intensos de frutas rojas (frambuesa, cereza) y una potente ciruela negra, así como tonos florales de la gama de las rosas y violetas, vegetales muy elegantes y complementarios, hojarasca verde, tabaco y maderas aromáticas (sándalo) que le brindan un toque ciertamente perfumado."
+                                           origin:@"El Bierzo"
+                                            photo:winePhoto
+                                   wineCompanyWeb:wineCompanyWeb
+                                           grapes:@[@"Mencía"]
+                                           rating:5];
+    
+    WineViewController* wineMainController = [[WineViewController alloc] initWithModel: bembibre];
+    
+    self.window.rootViewController = wineMainController;
     
     return YES;
 }
