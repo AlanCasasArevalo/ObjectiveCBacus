8fd2695ce95b8d34306e733f2334dc98a764fd0f
Unknown <alancasasarevalo@gmail.com>
2018-12-27 22:14:42 +0100

    [Evolution]
    Winery model was added
    WineryTable was added but is empty

---
+++ ObjectiveCBacus/Controllers/WineryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WineryTableViewController.h
+//  ObjectiveCBacus
+//
+//  Created by Alan Casas on 27/12/18.
+//  Copyright © 2018 Alan Casas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WineryTableViewController : UITableViewController
+
+@end
